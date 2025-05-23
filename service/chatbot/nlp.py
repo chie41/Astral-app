65aa3de3226463986847766d0c57c116cb4b0230
chie41 <91408610+chie41@users.noreply.github.com>
2025-05-24 05:44:53 +0700

    Del

--- service/chatbot/nlp.py
+++
@@ -1,9 +0,0 @@
-def analyze_message(text: str):
-    # Gọi AI hoặc xử lý text thô
-    # Ví dụ trả về loại bài toán và câu trả lời đơn giản
-    if "ảnh" in text or "image" in text:
-        return "image classification", "Bạn chọn Image Classification."
-    elif "text" in text:
-        return "text classification", "Bạn chọn Text Classification."
-    else:
-        return "tabular classification", "Bạn chọn Tabular Classification."
